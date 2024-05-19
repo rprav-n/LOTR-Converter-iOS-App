bbcc0165ff2f39c56c4dba0edd1d502266b37140
Praveen <rpraveen.engineer@gmail.com>
2024-05-19 21:48:22 +0530

    Added the main screen with text fields

---
+++ LOTR Converter/LOTR Converter/ContentView.swift
@@ -0,0 +1,107 @@
+//
+//  ContentView.swift
+//  LOTR Converter
+//
+//  Created by Praveen R on 19/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var showExchangeInfo = false
+    @State var leftAmount = ""
+    @State var rightAmount = ""
+    
+    var body: some View {
+        ZStack {
+            // Background image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                // Prancing pony image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                // Currency exchange text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                
+                // Currency conversion
+                HStack {
+                    // Left convertion section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
+                            // Currency text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        }
+                        // Textfield
+                        TextField("Text", text: $leftAmount)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    // Equal sign
+                    Image(systemName: "equal")
+                        .font(.title)
+                        .foregroundColor(.white)
+                    
+                    // Right convertion section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                            // Currency image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                        }
+                        // Textfield
+                        TextField("Text", text: $rightAmount)
+                            .textFieldStyle(.roundedBorder)
+                            .multilineTextAlignment(.trailing)
+                    }
+                }
+                .padding()
+                .background(.black.opacity(0.5))
+                .clipShape(.capsule)
+                
+                Spacer()
+                
+                // Info button
+                HStack {
+                    Spacer()
+                    Button {
+                        showExchangeInfo.toggle()
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                    }
+                    .padding(.trailing)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
